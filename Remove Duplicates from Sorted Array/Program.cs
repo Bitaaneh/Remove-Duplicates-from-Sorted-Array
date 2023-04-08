d44b88b4dae024472664f9405889b0ee04921cdd
Bita Rahimkhani <taaghche.bitarahimkhani@gmail.com>
2023-04-08 23:26:56 +0430

    Question Added

--- Remove Duplicates from Sorted Array/Program.cs
+++ Remove Duplicates from Sorted Array/Program.cs
@@ -1,5 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Problem 
+//Given an integer array nums sorted in non-decreasing order,
+//remove the duplicates in-place such that each unique element appears only once.
+//The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+//Consider the number of unique elements of nums be k, to get accepted, you need to do the following things:
+//Change the array nums such that the first k elements of nums contain the unique elements in the order they were
+//present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+#endregion 
+
+
+#region solution
 
 //var tmp = new int[10]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
 var tmp = new int[3]{0, 0, 1};
@@ -36,3 +45,4 @@ RemoveDuplicates(tmp);
 
     return counter;
 }
+#endregion
